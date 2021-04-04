0a46a81b7bcbccbe9ad437d297af2f390acb660d
storm-sergey <st.sergey.a@gmail.com>
2021-04-04 03:42:29 +0300

    ref: codestyle

--- test_f.c
+++ test_f.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <wchar.h>
 
-
-double foo1(short i) {
-   return(double)i;
+double foo1(short i)
+{
+   return (double) i;
 }
 
-const wchar_t* foo2(const wchar_t* s) {
+const wchar_t* foo2(const wchar_t* s)
+{
    const wchar_t* test_s = s;
    return test_s;
 }
 
-void* foo3(double d, int l, const wchar_t* s) {
+void* foo3(double d, int l, const wchar_t* s)
+{
    printf("\"first param\"=%f, \"second param\"=%d, ", d, l);
    wprintf(L"\"third param\"=\"%ls\"\n", s);
-   return(void*)s;
+   return (void*) s;
 }
 
-void foo4(int rows, int array[rows]) {
+void foo4(int rows, int array[rows])
+{
     printf("%d \n", rows);
-    for (int i = 0; i < rows; i++) {
+    for (int i = 0; i < rows; i++)
+    {
         printf("%d ", array[i]);
     }
 }
